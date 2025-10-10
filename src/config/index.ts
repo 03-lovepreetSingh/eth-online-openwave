00a13666881970e3b266c2b9f8a1665e7896f15a
Lovepreet Singh <135840741+03-lovepreetSingh@users.noreply.github.com>
2025-10-10 19:57:12 +0530

    added the U2U mainnet and updated the smarty contract

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,49 +2,11 @@ import { cookieStorage, createStorage } from "wagmi";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 
 // Get projectId from environment variables with fallback
-export const projectId = `ff7e4c6da87929d965ceb31b6a72924c`;
+export const projectId = process.env.REOWN_PROJECT_ID || "ff7e4c6da87929d965ceb31b6a72924c";
+console.log("Project ID:", projectId , process.env.REOWN_PROJECT_ID); 
 
-export const U2UTestnet = {
-  id: 545,
-  name: "U2U EVM Testnet",
-  chainNamespace: "eip155",
-  nativeCurrency: { name: "U2U", symbol: "U2U", decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ["https://testnet.evm.nodes.onU2U.org/"]
-    },
-    public: {
-      http: ["https://testnet.evm.nodes.onU2U.org/"],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: "U2U Testnet Explorer",
-      url: "https://evm-testnet.U2Uscan.io/",
-    },
-  },
-};
 
-export const avalancheTestnet = {
-  id: 43113,
-  name: "Avalanche Fuji C-Chain",
-  chainNamespace: "eip155",
-  nativeCurrency: { name: "U2U", symbol: "U2U", decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ["https://api.U2U-test.network/ext/bc/C/rpc"]
-    },
-    public: {
-      http: ["https://api.U2U-test.network/ext/bc/C/rpc"],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: "Avalanche Testnet Scan",
-      url: "https://subnets-test.U2U.network/c-chain",
-    },
-  },
-}
+
 
 export const u2uTestnet = {
   id: 2484,
@@ -67,8 +29,28 @@ export const u2uTestnet = {
     },
   },
 }
+export const U2UMainnet = {
+  id: 39,
+  name: "U2U Network Solaris",
+  chainNamespace: "eip155",
+  nativeCurrency: { name: "U2U", symbol: "U2U", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc-mainnet.u2u.xyz", "https://rpc-tracer-mainnet.u2u.xyz"]
+    },
+    public: {
+      http: ["https://rpc-mainnet.u2u.xyz", "https://rpc-tracer-mainnet.u2u.xyz"]
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "U2U Mainnet Explorer",
+      url: "https://u2uscan.xyz",
+    },
+  },
+}
 
-export const networks = [U2UTestnet, avalancheTestnet, u2uTestnet];
+export const networks = [ u2uTestnet, U2UMainnet];
 
 // Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
